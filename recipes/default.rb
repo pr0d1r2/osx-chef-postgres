abf461426abf1fb59dd87c07f0b6d0e5957f679c
Marcin Nowicki <pr0d1r2@gmail.com>
2016-05-20 17:38:13 +0200

    Support for chefdk kitchenplan config

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,29 @@
 include_recipe "homebrewalt::default"
 
+if node['user'] && node['user']['id']
+  user_name = node['user']['id']
+  home_dir = Etc.getpwnam(user_name).dir
+else
+  user_name = node['current_user']
+  home_dir = node['etc']['passwd'][user_name]['dir']
+end
+
 ["homebrew.mxcl.postgresql.plist" ].each do |plist|
-  plist_path = File.expand_path(plist, File.join('~', 'Library', 'LaunchAgents'))
+  plist_path = File.expand_path(plist, File.join(home_dir, 'Library', 'LaunchAgents'))
   if File.exists?(plist_path)
     log "postgresql plist found at #{plist_path}"
     execute "unload the plist (shuts down the daemon)" do
       command %'launchctl unload -w #{plist_path}'
-      user node['current_user']
+      user user_name
     end
   else
     log "Did not find plist at #{plist_path} don't try to unload it"
   end
 end
 
-[ "/Users/#{node['current_user']}/Library/LaunchAgents" ].each do |dir|
+[ "/Users/#{user_name}/Library/LaunchAgents" ].each do |dir|
   directory dir do
-    owner node['current_user']
+    owner user_name
     action :create
   end
 end
@@ -26,12 +34,12 @@ end
 
 execute "copy over the plist" do
     command %'cp /usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/'
-    user node['current_user']
+    user user_name
 end
 
 execute "start the daemon" do
   command %'launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
-  user node['current_user']
+  user user_name
 end
 
 ruby_block "Checking that postgres is running" do
@@ -47,7 +55,7 @@ end
 [ node['postgres_users'] ].flatten.compact.each do |postgres_user|
   execute "Creating postgres user: #{postgres_user}" do
     command "createuser -s #{postgres_user}"
-    user node['current_user']
+    user user_name
     not_if { system("psql postgres -tAc \"SELECT 1 FROM pg_roles WHERE rolname='#{postgres_user}'\" | grep -q 1") }
   end
 end
